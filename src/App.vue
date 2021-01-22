f58e275f80c54582a4bed2feb4fa13681dbaf10a
Yannik195 <yannisim@gmail.com>
2021-01-22 17:22:16 +0100

    post

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <v-app>
-    <div>
-      <v-text-field label="Solo" placeholder="Placeholder" solo></v-text-field>
-      <v-btn elevation="2">post</v-btn>
-      {{ posts }}
+  <v-app class="container">
+    <div class="ma-4">
+      <div class="input">
+        <v-text-field
+          v-model="text"
+          class="mr-4"
+          label="Post something"
+          single-line
+          outlined
+        ></v-text-field>
+        <v-btn @click="post" elevation="0" color="primary" x-large>post</v-btn>
+      </div>
+
+      <Post
+        class="pb-4"
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+      ></Post>
     </div>
   </v-app>
 </template>
 
 <script>
 import axios from "axios";
+import Post from "./components/Post";
 
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    Post,
+  },
 
   data: () => ({
     posts: null,
+    text: "",
   }),
+  methods: {
+    post: function () {
+      axios
+        .post("https://hidden-earth-37796.herokuapp.com/posts", {
+          text: this.text,
+          userName: "Ich",
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
   mounted() {
     axios
-      .get("https://hidden-earth-37796.herokuapp.com/posts")
+      .get("https://hidden-earth-37796.herokuapp.com/new-posts")
       .then((result) => {
         this.posts = result.data;
       })
@@ -31,3 +64,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 60em;
+  margin: 0 auto;
+}
+
+.content {
+}
+
+.input {
+  display: flex;
+  align-items: baseline;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ post.userName }}</p>
+    <p>{{ post.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: ["post"],
+  data: () => ({}),
+  mounted() {},
+};
+</script>
+
+<style scoped>
+p {
+  margin: 0;
+}
+</style>
